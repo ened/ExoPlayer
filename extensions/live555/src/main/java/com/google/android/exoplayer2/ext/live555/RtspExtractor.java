505fdcc14fe83f2035f9a74acb66a2f62fdf40ff
Sebastian Roth <sebastian.roth@gmail.com>
2016-08-04 17:06:32 +0800

    Add various methods to push H.264 frames into the decoder

--- extensions/live555/src/main/java/com/google/android/exoplayer2/ext/live555/RtspExtractor.java
+++ extensions/live555/src/main/java/com/google/android/exoplayer2/ext/live555/RtspExtractor.java
@@ -2,22 +2,30 @@ package com.google.android.exoplayer2.ext.live555;
 
 import android.util.Log;
 
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.Format;
 import com.google.android.exoplayer2.extractor.Extractor;
 import com.google.android.exoplayer2.extractor.ExtractorInput;
 import com.google.android.exoplayer2.extractor.ExtractorOutput;
 import com.google.android.exoplayer2.extractor.PositionHolder;
 import com.google.android.exoplayer2.extractor.SeekMap;
+import com.google.android.exoplayer2.extractor.TrackOutput;
+import com.google.android.exoplayer2.util.MimeTypes;
 import com.google.android.exoplayer2.util.NalUnitUtil;
 import com.google.android.exoplayer2.util.ParsableByteArray;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class RtspExtractor implements Extractor {
 
     private final static String TAG = RtspExtractor.class.getSimpleName();
+    public static final int SPS_LENGTH = 21;
+
     private final boolean[] prefixFlags;
 
-    private ExtractorOutput output;
+    private ExtractorOutput extractorOutput;
+    private TrackOutput trackOutput;
 //    private H264Reader reader;
 
     public RtspExtractor() {
@@ -26,56 +34,75 @@ public class RtspExtractor implements Extractor {
 
     @Override
     public void init(ExtractorOutput output) {
-        this.output = output;
+        Log.d(TAG, "init() called with: extractorOutput = [" + output + "]");
 
-        output.seekMap(new SeekMap.Unseekable(1));
+        this.extractorOutput = output;
+        trackOutput = extractorOutput.track(0);
+        extractorOutput.endTracks();
+
+
+        output.seekMap(new SeekMap.Unseekable(C.UNSET_TIME_US));
     }
 
     @Override
     public boolean sniff(ExtractorInput input) throws IOException, InterruptedException {
-        byte[] data = new byte[1024];
+        Log.d(TAG, "sniff() called with: input = [" + input + "]");
 
-        input.read(data, 0, data.length);
+        byte[] header = new byte[RtspDataSource.FAKE_HEADER.length];
+        input.peekFully(header, 0, RtspDataSource.FAKE_HEADER.length);
+        if (Arrays.equals(header, RtspDataSource.FAKE_HEADER)) {
+            byte[] data = new byte[SPS_LENGTH];
+            input.peekFully(data, 0, data.length);
 
-        NalUnitUtil.SpsData spsData = NalUnitUtil.parseSpsNalUnit(data, 0, 100);
+            NalUnitUtil.SpsData spsData = NalUnitUtil.parseSpsNalUnit(data, 0, data.length);
 
-        if (spsData != null) {
-            Log.d(TAG, "sniff: width, height: " + spsData.width + "," + spsData.height);
-            return true;
+            if (spsData != null) {
+                Log.d(TAG, "sniff: width, height: " + spsData.width + "," + spsData.height);
+                return true;
+            }
         }
 
         return false;
     }
 
-
     @Override
     public int read(ExtractorInput input, PositionHolder seekPosition) throws IOException, InterruptedException {
-
         byte[] data = new byte[4096];
 
-        int size = input.read(data, 0, data.length);
+        input.resetPeekPosition();
 
-        ParsableByteArray array = new ParsableByteArray(data);
+        byte[] header = new byte[RtspDataSource.FAKE_HEADER.length];
+        input.peekFully(header, 0, RtspDataSource.FAKE_HEADER.length);
+        if (Arrays.equals(header, RtspDataSource.FAKE_HEADER)) {
 
-//        if (reader == null) {
-//            TrackOutput track = output.track(0);
-//            NalUnitUtil.SpsData spsData = NalUnitUtil.parseSpsNalUnit(data, 0, 100);
-//
-//            if (spsData == null) {
-//                return RESULT_END_OF_INPUT;
-//            }
-//
-////            reader = new H264Reader(track, new SeiReader(track), true, true);
-//        }
+            input.skip(4);
+            input.read(data, 0, SPS_LENGTH);
 
-//        reader.consume(array);
+            try {
+                NalUnitUtil.SpsData spsData = NalUnitUtil.parseSpsNalUnit(data, 0, SPS_LENGTH);
+
+                trackOutput.format(Format.createVideoSampleFormat(null, MimeTypes.VIDEO_H264, null,
+                        Format.NO_VALUE, Format.NO_VALUE, spsData.width, spsData.height, Format.NO_VALUE,
+                        null, Format.NO_VALUE, spsData.pixelWidthAspectRatio, null));
+            } catch (IllegalStateException e) {
+                Log.e(TAG, "read: ", e);
+                return RESULT_CONTINUE;
+            }
+        }
+
+
+        int sizeRead = input.read(data, 0, data.length);
+
+        ParsableByteArray parsableByteArray = new ParsableByteArray(data);
+        trackOutput.sampleData(parsableByteArray, sizeRead);
+        trackOutput.sampleMetadata(0, 0, sizeRead, 0, null);
 
         return RESULT_CONTINUE;
     }
 
     @Override
     public void seek(long position) {
-
+        Log.d(TAG, "seek() called with: position = [" + position + "]");
     }
 
     @Override
